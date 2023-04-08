01407631ab9876a754007cd2a4701cef5cfc9010
Daniel <danireysan@gmail.com>
2023-04-08 12:01:22 -0600

    added a new function

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,15 +1,19 @@
-const { io } = require('../index');
+const { io } = require("../index");
 // Socket messages
 
-io.on('connection', client => {
-    console.log('Client connected...');
-    client.on('disconnect', () => { 
-      console.log('Client disconnected') 
-    });
-  
-    client.on('message', (payload) => {
-      console.log('Message', payload);
-  
-      io.emit('message', { admin: 'New message' });
-    });
+io.on("connection", (client) => {
+  console.log("Client connected...");
+  client.on("disconnect", () => {
+    console.log("Client disconnected");
   });
+
+  client.on("message", (payload) => {
+    console.log("Message", payload);
+
+    io.emit("message", { admin: "New message" });
+  });
+
+  client.on("emit-message", function (payload) {
+    io.emit("new-message", payload);
+  });
+});
