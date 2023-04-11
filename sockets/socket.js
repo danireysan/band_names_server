79819cf9eea56076138458f66406645c33a5f838
Daniel <danireysan@gmail.com>
2023-04-11 17:05:52 -0600

    added vote band

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,22 @@
 const { io } = require("../index");
 // Socket messages
 
+const Bands = require("../models/bands");
+const bands = new Bands();
+const Band = require("../models/band");
+bands.addBand(new Band("Queen"));
+bands.addBand(new Band("Death"));
+bands.addBand(new Band("Tom Cardi"));
+bands.addBand(new Band("Eminem"));
+
+console.log(bands);
+
+
 io.on("connection", (client) => {
   console.log("Client connected...");
+
+  client.emit("active-bands", bands.getBands());
+  
   client.on("disconnect", () => {
     console.log("Client disconnected");
   });
@@ -15,5 +29,10 @@ io.on("connection", (client) => {
 
   client.on("emit-message", function (payload) {
     client.broadcast.emit("new-message", payload);
+  });
+
+  client.on("vote-band", (payload) => {
+    bands.voteBand(payload.id);
+    io.emit("active-bands", bands.getBands());
   });
 });
